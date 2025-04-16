31bfb81e508a158b4fdbdc40c0d1ef6d872dd728
giobocca03 <gio.bocca03@gmail.com>
2025-04-16 17:15:42 +0200

    aggiunto metodo delete prodotti

--- routes/prodotti.js
+++ routes/prodotti.js
@@ -60,4 +60,20 @@ router.put('/:id', async (req, res) => {
 })
 
 
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const rowsAffected = await db('prodotti_venduti').where('id', id).delete()
+    if (rowsAffected === 0) {
+      return res.status(404).json({ err: 'Il prodotto non esiste' })
+    }
+    res.status(200).json({ message: 'Prodotto eliminato con successo' });
+  } catch (err) {
+    console.error("Errore nella cancellazione del prodotto:", err);
+    res.status(500).json({ err: 'Errore interno del server' });
+  }
+
+})
+
+
 module.exports = router;
