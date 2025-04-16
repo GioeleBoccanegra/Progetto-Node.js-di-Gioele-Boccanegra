f5755b7a8b91cdcc18b61b65554851adeee97fdd
giobocca03 <gio.bocca03@gmail.com>
2025-04-16 17:11:42 +0200

    aggiunto metodo put prodotti

--- routes/prodotti.js
+++ routes/prodotti.js
@@ -30,5 +30,34 @@ router.post('/', async (req, res) => {
   }
 })
 
+router.put('/:id', async (req, res) => {
+  const { id } = req.params
+  const { nome, numero_foto } = req.body
+
+  if (!nome) {
+    return res.status(400).json({ err: 'Il campo "nome" è obbligatorio' })
+  } else if (!numero_foto) {
+    return res.status(400).json({ err: 'Il campo "numero_foto" è obbligatorio' })
+  }
+
+  try {
+    //crea oggetto dati da aggiungere
+    const updateProdotto = {}
+    if (nome) updateProdotto.nome = nome
+    if (numero_foto) updateProdotto.numero_foto = numero_foto
+
+    //esegue aggiornamento:
+    const rowsAffected = await db('prodotti_venduti').where('id', id).update(updateProdotto)
+
+    if (rowsAffected === 0) {
+      return res.status(404).json({ err: 'Il prodotto non esiste' })
+    }
+    res.status(200).json({ id, ...updateProdotto });
+  } catch (err) {
+    console.error("Errore nell'aggiornamento del prodotto:", err);
+    res.status(500).json({ err: 'Errore interno del server' });
+  }
+})
+
 
 module.exports = router;
